0e99f52fade625282a8cff1434bde88008e01d4a
eamontori <emmanuelamontori46@gmail.com>
2024-10-01 22:32:48 -0400

    Added lab2e

---
+++ OPS445/lab2/lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Prince Adu-Adjei
+# Author ID: 112407226
+# Date Created: 2024/05/22
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer -= 1
+print('blast off!')

69f03f22798a87e2a06aa8aa92d25e747fffa94e
eamontori <emmanuelamontori46@gmail.com>
2024-10-01 22:52:56 -0400

    Added lab2g

---
+++ OPS445/lab2/lab2g.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: EMMANUEL AMO-NTORI
+# Author ID: 113423222
+# Date Created: 2024/10/01
+
+import sys
+
+if len(sys.argv) > 1:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer != 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
